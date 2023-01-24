d2a3ea06237d660d93d8a2b712a1116cf1254e55
Foxar <mostek3pl@gmail.com>
2023-01-24 17:10:38 +0100

    feature: implemented the store in settings page password component

--- frontend/meaner-ui/src/app/modules/settings/components/settings-password-change/settings-password-change.component.ts
+++ frontend/meaner-ui/src/app/modules/settings/components/settings-password-change/settings-password-change.component.ts
@@ -1,5 +1,10 @@
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component, OnDestroy } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { tap } from "rxjs";
+import { AppState } from "src/app/state/app.state";
+import { changePassword } from "src/app/state/settings/settings.action";
+import { selectPasswordError, selectPasswordStatus } from "src/app/state/settings/settings.selector";
 import { confirmPasswordValidator } from "src/app/validators/confirmPasswordValidator";
 
 @Component({
@@ -7,10 +12,28 @@ import { confirmPasswordValidator } from "src/app/validators/confirmPasswordVali
     templateUrl: './settings-password-change.component.html',
     styleUrls: ['./settings-password-change.component.scss']
 })
-export class SettingsPasswordChangeComponent {
+export class SettingsPasswordChangeComponent implements OnDestroy {
     constructor(
         private fb: FormBuilder,
+        private store: Store<AppState>,
+        private changeDetectorRef: ChangeDetectorRef,
     ){}
+
+    settingsPageStatus$ = this.store.select(selectPasswordStatus).pipe(
+      tap((status) => {
+        console.log("Settings password status: " + status);
+      })
+    );
+
+    settingsPageError$ = this.store.select(selectPasswordError).pipe(
+      tap((error) => {
+        console.log("Settings password error: " + error);
+        if(error && error == 'Invalid password'){
+          this.changePasswordForm.controls["currentPassword"].setErrors({'invalid': true});
+          this.changeDetectorRef.detectChanges();
+        }
+      })
+    ).subscribe();
 
 
     changePasswordForm: FormGroup = this.fb.group({
@@ -45,5 +68,17 @@ export class SettingsPasswordChangeComponent {
       onSave(): void {
         this.changePasswordForm.markAllAsTouched();
         this.changePasswordForm.updateValueAndValidity();
+
+
+        if(this.changePasswordForm.valid){
+          this.store.dispatch(changePassword({
+            newPassword: this.changePasswordForm.value["newPassword"],
+            oldPassword: this.changePasswordForm.value["currentPassword"]
+          }))
+        }
+      }
+
+      ngOnDestroy(): void {
+        this.settingsPageError$.unsubscribe();
       }
 }
