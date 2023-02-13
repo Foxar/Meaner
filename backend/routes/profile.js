456eb79827ae5fe7d0fd87f91d917ed51916aff3
Foxar <mostek3pl@gmail.com>
2023-02-13 17:17:24 +0100

    feature: generic param validator/bson validation

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const profileController = require('../controllers/profileController');
+const { paramValidatorMiddleware } = require('../middleware/paramValidators');
 
 router
     .get('/:id', profileController.getProfileById)
-    .get('/user/:id', profileController.getProfileByUserId)
+    .get('/user/:id',paramValidatorMiddleware('bson','id'), profileController.getProfileByUserId)
 
 module.exports = router;
