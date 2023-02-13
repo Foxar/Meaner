c301164dd81d9bb6d0f09cb2f46d50cd57df6005
Foxar <mostek3pl@gmail.com>
2023-02-13 17:19:55 +0100

    feature: BSON validation for other routes

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -3,7 +3,7 @@ const profileController = require('../controllers/profileController');
 const { paramValidatorMiddleware } = require('../middleware/paramValidators');
 
 router
-    .get('/:id', profileController.getProfileById)
+    .get('/:id', paramValidatorMiddleware('bson','id'), profileController.getProfileById)
     .get('/user/:id',paramValidatorMiddleware('bson','id'), profileController.getProfileByUserId)
 
 module.exports = router;
--- backend/routes/tweet.js
+++ backend/routes/tweet.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const tweetController = require('../controllers/tweetController');
 const authController = require('../controllers/authController');
 const { authMiddleware } = require('../middleware/auth');
+const { paramValidatorMiddleware } = require('../middleware/paramValidators');
 
 router
     .get('/home/:offset', tweetController.getHomeTweets)
-    .get('/:id', tweetController.getTweet)
+    .get('/:id', paramValidatorMiddleware('bson','id'), tweetController.getTweet)
     .post('/', authMiddleware, tweetController.postTweet)
-    .put('/like/:id', authMiddleware, tweetController.likeTweet)
-    .get('/replies/:id', tweetController.getReplies)
-    .get('/user/:id', tweetController.getUserTweets)
-    .get('/like/:userId/:tweetId', tweetController.likeTweet)
+    .put('/like/:id', paramValidatorMiddleware('bson','id'), authMiddleware, tweetController.likeTweet)
+    .get('/replies/:id', paramValidatorMiddleware('bson','id'), tweetController.getReplies)
+    .get('/user/:id', paramValidatorMiddleware('bson','id'), tweetController.getUserTweets)
+    .get('/like/:userId/:tweetId', paramValidatorMiddleware('bson','id'), tweetController.likeTweet)
     // .delete('/:id',tweetController.deleteTweet)
 
 module.exports = router; 
