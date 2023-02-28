857c8cf1ed701e99dbb0648eb851d786652eef7c
Foxar <mostek3pl@gmail.com>
2023-02-28 14:32:44 +0100

    feature: ngrx integration for descritptions

--- frontend/meaner-ui/src/app/modules/settings/components/profile-change/profile-change.component.ts
+++ frontend/meaner-ui/src/app/modules/settings/components/profile-change/profile-change.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs';
+import { of, switchMap, tap } from 'rxjs';
+import { MeanerApiService } from '../../../../services/meanerApi.service';
 import { AppState } from '../../../../state/app.state';
+import { changeDescription } from '../../../../state/settings/settings.action';
 import { selectPasswordStatus } from '../../../../state/settings/settings.selector';
+import { selectUser } from '../../../../state/user/user.selector';
 
 @Component({
   selector: 'app-profile-change',
   templateUrl: './profile-change.component.html',
   styleUrls: ['./profile-change.component.scss']
 })
-export class ProfileChangeComponent {
+export class ProfileChangeComponent implements OnDestroy {
   constructor(
     private fb: FormBuilder,
     private store: Store<AppState>,
+    private meanerApi: MeanerApiService,
 ){}
 
     //Move this from here and password change element to the page, pass through Input. Keep code DRY.
@@ -22,6 +26,19 @@ export class ProfileChangeComponent {
       console.log("Settings password status: " + status);
     })
   );
+
+  currentProfileDesc$ = this.store.select(selectUser).pipe(
+    tap((user) => {
+      console.log(user);
+
+    }),
+    switchMap((user) => {
+      return user ? this.meanerApi.getProfileByUserId(user.id) : of();
+    })
+  ).subscribe(profileResponse => {
+    if(this.changeProfileForm.pristine)
+       this.changeProfileForm.controls['description'].setValue(profileResponse.description);
+  })
 
   changeProfileForm: FormGroup = this.fb.group({
     description: [
@@ -36,5 +53,12 @@ export class ProfileChangeComponent {
 
   onSave(): void {
     this.changeProfileForm.markAllAsTouched();
+    if(this.changeProfileForm.valid){
+      this.store.dispatch(changeDescription({description: this.changeProfileForm.controls['description'].value}));
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.currentProfileDesc$.unsubscribe();
   }
 }
